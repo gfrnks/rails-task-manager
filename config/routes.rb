431549329a1119e19cb156c9eea6ec657bfb98fa
Gary Franks <gary.udle@gmail.com>
2019-02-12 15:59:27 -0500

    ex1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "tasks/new", to: "tasks#new", as: :new_task
+  get "tasks", to: "tasks#index", as: :tasks
+  get "tasks/:id", to: "tasks#show", as: :task
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  post "tasks", to: "tasks#create"
+  patch "tasks/:id", to: "tasks#update"
+  patch "tasks/:id/mark", to: "tasks#mark", as: :mark_task
+  delete "tasks/:id", to: "tasks#destroy", as: :destroy_task
 end
+
+# One route is there to display the Task form
+
+# One is there to handle the POST request generated
+# when submitting this form.
+
+
+
+
+
+
